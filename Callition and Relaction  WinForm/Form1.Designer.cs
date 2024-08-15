2ffc13bfb8c4f8384a749c2a6f7ac57e483cece3
Gorobrat <gor.melqonyan.2013@gmail.com>
2024-08-15 17:04:26 +0400

    second commit

---
+++ Callition and Relaction  WinForm/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿
+namespace Callition_and_Relaction__WinForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.player = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.mainTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // player
+            // 
+            this.player.BackColor = System.Drawing.Color.Green;
+            this.player.Location = new System.Drawing.Point(255, 217);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(74, 66);
+            this.player.TabIndex = 0;
+            this.player.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.DarkBlue;
+            this.pictureBox2.Location = new System.Drawing.Point(41, 371);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(74, 66);
+            this.pictureBox2.TabIndex = 0;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Tag = "object";
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.DarkBlue;
+            this.pictureBox3.Location = new System.Drawing.Point(41, 53);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(74, 66);
+            this.pictureBox3.TabIndex = 0;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Tag = "object";
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.Color.DarkBlue;
+            this.pictureBox4.Location = new System.Drawing.Point(480, 53);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(74, 66);
+            this.pictureBox4.TabIndex = 0;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Tag = "object";
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.BackColor = System.Drawing.Color.DarkBlue;
+            this.pictureBox5.Location = new System.Drawing.Point(480, 371);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(74, 66);
+            this.pictureBox5.TabIndex = 0;
+            this.pictureBox5.TabStop = false;
+            this.pictureBox5.Tag = "object";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(41, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(137, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "You Hit with -??";
+            // 
+            // mainTimer
+            // 
+            this.mainTimer.Enabled = true;
+            this.mainTimer.Interval = 20;
+            this.mainTimer.Tick += new System.EventHandler(this.mainTimerEvent);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(624, 486);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.player);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.keyisDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.keyisUp);
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer mainTimer;
+    }
+}
+
---
+++ Callition and Relaction  WinForm/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Callition_and_Relaction__WinForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        bool moveLeft, moveRight, moveUp, moveDown;
+        int speed = 8;
+        private void mainTimerEvent(object sender, EventArgs e)
+        {
+            if (moveLeft == true && player.Left > 0)
+            {
+                player.Left -= speed;
+            }
+            if (moveRight == true && player.Right < 620)
+            {
+                player.Left += speed;
+            }
+            if (moveUp == true && player.Top > 4)
+            {
+                player.Top -= speed;
+            }
+            if (moveDown == true && player.Top < 419)
+            {
+                player.Top += speed;
+            }
+            foreach (Control x in this.Controls)
+            {
+                if (x is PictureBox && (string)x.Tag == "object")
+                {
+                    if (player.Bounds.IntersectsWith(x.Bounds))
+                    {
+                        x.BackColor = Color.Red;
+                        label1.Text = "You hit with " + x.Name;
+                    }
+                    else
+                    {
+                        x.BackColor = Color.DarkBlue;
+                    }
+                }
+            }
+        }
+        private void keyisDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                moveLeft = true;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                moveRight = true;
+            }
+            if (e.KeyCode == Keys.Up)
+            {
+                moveUp = true;
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                moveDown = true;
+            }
+        }
+        private void keyisUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                moveLeft = false;
+            }
+            if (e.KeyCode == Keys.Right)
+            {
+                moveRight = false;
+            }
+            if (e.KeyCode == Keys.Up)
+            {
+                moveUp = false;
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                moveDown = false;
+            }
+        }
+    }
+}
